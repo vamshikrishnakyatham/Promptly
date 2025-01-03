b5b0e4de5572f67c56e22135c2f0dfa302fe081d
Vamshi Krishna Kyatham <68469412+vamshikrishnakyatham@users.noreply.github.com>
2025-01-02 21:33:40 -0500

    Promptly v1

--- models/user.js
+++ models/user.js
@@ -11,6 +11,11 @@ const UserSchema = new Schema({
         required: [true, 'Username is required!'],
         // match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
     },
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+        // match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    },
     image: {
         type: String
     }
